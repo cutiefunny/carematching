bed5f1c1ed46edb0c1c84c45b83a86334b01408f
cutiefunny <cutiefunny@gmail.com>
2025-07-21 00:59:38 +0900

    rebuild

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
-/** @type {import('next').NextConfig} */
+import withPWA from "@ducanh2912/next-pwa";
+
 const nextConfig = {
+    reactStrictMode: true,
     images: {
     domains: ['placehold.co'], // 여기에 placehold.co를 추가합니다.
   },
